6e46c1f162ca5bff1493dc5d498120ecc6e0a325
Shreyas Vaidya <95679215+Shreyasvaidya@users.noreply.github.com>
2022-09-15 23:47:03 +0530

    removed comments and converted to function

--- Voice to Text (GTTS).py
+++ Voice to Text (GTTS).py
@@ -1,38 +1,33 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
-pip install GTTS
+def text_to_speech(text,lang):
+        from playsound import playsound
+        from gtts import gTTS
 
 
-# In[11]:
+
+        import os
 
 
-from gtts import gTTS 
-  
-# This module is imported so that we can 
-# play the converted audio
-import os
-  
-# The text that you want to convert to audio
-mytext = 'There was a girl. She loved to play.There were many games but did not have great fun. Let us begin our story.'
-  
+        #mytext = 'There was a girl. She loved to play.There were many games but did not have great fun. Let us begin our story.'
+
 # Language in which you want to convert
-language = 'en'  #Check the gtts documentation to get lang codes
-  
+        language = lang  #Check the gtts documentation to get lang codes
+
 # Passing the text and language to the engine, 
 # here we have marked slow=False. Which tells 
 # the module that the converted audio should 
 # have a high speed
-myobj = gTTS(text=mytext, lang=language, slow=False)
-  
+        myobj = gTTS(text=mytext, lang=language, slow=False)
+
 # Saving the converted audio in a mp3 file named
 # welcome 
-myobj.save("msg.mp3")
-  
-# Playing the converted file
-os.system("start msg.mp3")
-#os.system("mpg321 msg.mp3")
+        myobj.save("msg.mp3")
 
+# Playing the converted file
+        playsound("msg.mp3")
+        return
+#os.system("mpg321 msg.mp3")
+#test
+
+mytext = 'There was a girl. She loved to play.There were many games but did not have great fun. Let us begin our story.'
+text_to_speech(mytext,'en')
+~                                  
